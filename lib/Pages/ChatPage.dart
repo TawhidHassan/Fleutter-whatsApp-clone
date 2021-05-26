ac19fe3db9d0a9aa392de6180a087de75baadff5
sifat hassan <sifat007.sh7@gmail.com>
2021-05-26 12:16:37 +0600

    Working on Select Contact page

--- lib/Pages/ChatPage.dart
+++ lib/Pages/ChatPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_whatsapp_clone/CustomUi/CustomCard.dart';
 import 'package:flutter_whatsapp_clone/Model/ChatModel.dart';
+import 'package:flutter_whatsapp_clone/Screens/SelectContactScreen.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -38,7 +39,7 @@ class _ChatPageState extends State<ChatPage> {
     return Scaffold(
      floatingActionButton: FloatingActionButton(
        onPressed: (){
-
+          Navigator.push(context, MaterialPageRoute(builder: (builder)=>SelectContact()));
        },
        child: Icon(Icons.chat),
      ),
